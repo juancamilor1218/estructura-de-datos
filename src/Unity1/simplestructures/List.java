d726408592b2a41a3c882a1dffd8e802c1e54a05
LEYDY <LEYDY@DESKTOP-VNFL2QQ>
2016-03-13 00:33:04 -0500

    improving

--- src/Unity1/simplestructures/List.java
+++ src/Unity1/simplestructures/List.java
@@ -100,4 +100,85 @@ public class List {
         }
         p.next=null;
     }
+     public void removeIntercaled(){
+       Node aux= head,p=head;
+        int c=Count();
+        for(int i=0;i<c;i++){
+            if(i%2==1){
+                p.next=aux.next;
+            }
+            p=aux;
+            aux=aux.next;
+        }
+    }
+    public void DuplicateIntercaled(){
+        Node aux= head,p=head;
+        int c=Count();
+        while (aux.next != null){
+            Node n=new Node(aux.data);
+            n.next=aux.next;
+            aux.next=n;
+            aux=n.next;
+        }
+    }
+    public void Invert(){
+        Node aux= head,head2=null;
+        while(head!=null){
+            aux=head;
+            head=head.next;
+            aux.next=head2.next;
+            head2.next=aux;
+        }
+        head=head2;
+    }
+    public void RemoveRecurrents(){
+        Node p =head, aux=head, back=head;
+        while(aux.next!=null){
+            p=head;
+            while(p!=aux){
+                if(p.data==aux.data){
+                    back.next=aux.next;
+                    back=aux;
+                }
+                p=p.next;
+            }
+            back=aux;
+            aux=aux.next;
+        }
+    }
+    
+    public void InsertZeros(){
+        Node aux=head;
+        int  c=Count();
+        for(int i=0;i<c-1;i++){
+            Node n=new Node(0);
+            n.next=aux.next;
+            aux.next=n;
+            aux=n.next;
+        }
+    }
+    
+    public void removeMiddle(){
+        Node aux= head , back =head;
+        int c=Count();
+        int half=c/2;
+        
+         for(int i=0;i<half;i++){
+             back=aux;
+             aux=aux.next;
+             
+         }
+         back.next=aux.next;   
+    }
+    public int countOdds(){
+        Node aux= head; 
+        int cont=0;
+        while(aux.next!=null){
+            if(aux.data%2!=0){
+                cont++;
+            }
+            aux=aux.next;
+        }
+        return cont;
+    }
 }
