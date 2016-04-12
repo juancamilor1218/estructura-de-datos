8ec2d4465d55beeae0f63646ce490e57b48d0ccc
AULA1 <AULA1@192.168.31.3>
2016-04-12 16:09:58 -0500

    graph

---
+++ src/Unity1/grapher/Interface.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unity1.grapher;
+
+/**
+ *
+ * @author AULA1
+ */
+public class Interface {
+    private Graph graph;
+    
+    
+    public Interface(){
+        
+    }
+    private void LaunchWidgets(){
+        
+    }
+    private void LaunchEvents(){
+        
+    }
+    
+   public static void main(String Args[]){
+       
+   }
+}
